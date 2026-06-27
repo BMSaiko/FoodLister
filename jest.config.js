a886edc99974b25579a09f7cbec143557373592b
Bruno Silva <1221514@isep.ipp.pt>
2026-06-27 17:18:18 +0100

    chore: test infrastructure cleanup and config restoration

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
@@ -28,5 +26,4 @@ const customJestConfig = {
   },
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
